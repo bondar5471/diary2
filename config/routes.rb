66e9e09902af10886a550bf507ed1ae0dd2ec61e
bondar5471 <bondar54711745@gmail.com>
2018-11-20 11:06:30 +0200

    generate list and card

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :cards
+  resources :lists
   root to: 'days#index'
 
   devise_for :user
---
+++ spec/factories/lists.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :list do
+    name "MyString"
+    position 1
+  end
+end
