9f2c1ef2f1afa24483c60e184f25540d943b4f4f
bondar5471 <bondar54711745@gmail.com>
2018-11-22 13:28:17 +0200

    fix link calendar day show create link calendar new day

--- app/helpers/days_helper.rb
+++ app/helpers/days_helper.rb
@@ -2,7 +2,6 @@
 
 module DaysHelper
   COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].freeze
-
   def days_in_month(month, year = Time.now.year)
     return 29 if month == 2 && Date.gregorian_leap?(year)
     COMMON_YEAR_DAYS_IN_MONTH[month]
@@ -11,15 +10,13 @@ module DaysHelper
   def formated_date(date)
     date.strftime('%B %d, %Y')
   end
-
-  def display_day(day_compar, days)
-    day_detected = days.detect { |d| day_compar == d.date.day }
-
+  def display_day(day_number, days)
+    day_detected = days.detect { |d| day_number == d.date.day }
     if day_detected
       color = day_detected.successful ? 'green' : 'red'
-      "<a href='/days/#{days[0]['id']}'><div class=\"#{color}\">#{day_detected.date.day} </div></a>".html_safe
+      "<a href='/days/#{day_detected['id']}'><div class=\"#{color}\" title='#{day_detected.date} \"#{color}\"'>#{day_detected.date.day}</div></a>".html_safe
     else
-      day_compar
+      "<a href='/days/new'><div  title='New day'>#{day_number}</div></a>".html_safe
     end
   end
 end
