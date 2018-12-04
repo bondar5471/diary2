e4cd6405cdc9fbcb91e3889997fed6c790447556
bondar5471 <bondar54711745@gmail.com>
2018-12-04 11:45:50 +0200

    add edit report on day drop not used method

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DaysController < ApplicationController
-  before_action :set_day, only: %i[show edit update destroy destroy_on_month]
+  before_action :set_day, only: %i[show edit update destroy]
   respond_to :html, :json
   # before_action :authenticate_user!
   def index
@@ -17,7 +17,6 @@ class DaysController < ApplicationController
 
   def create
     @day = Day.new(day_params)
-    # @day.skip_report_validation = true
     if @day.save
       flash[:success] = 'Day create.'
       redirect_to @day
@@ -27,15 +26,19 @@ class DaysController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    respond_to do |format|
+      format.html { @day.save }
+      format.js
+    end
+  end
 
   def update
     if @day.update(day_params)
-      flash[:success] = 'Day update.'
-      redirect_to @day
-    else
-      flash[:error] = 'Date and Report must be filled.'
-      render :edit
+      respond_to do |format|
+        format.html { redirect_to @day }
+        format.js
+      end
     end
   end
 
@@ -47,11 +50,6 @@ class DaysController < ApplicationController
       flash[:error] = 'Something went wrong'
       redirect_to days_url
     end
-  end
-
-  def destroy_on_month
-    Day.where('date < ?', 1.month.ago.end_of_month).destroy_all
-    redirect_to days_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,8 @@ Rails.application.routes.draw do
   end
 
   devise_for :user
-  resources :notices, only: %i[new create destroy]
   resources :days do
     resources :tasks
-    member do
-      delete :destroy_on_month
-    end
   end
   root to: 'days#index'
 end
