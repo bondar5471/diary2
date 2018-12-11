b7aeb9a6163929266c3b2d59f51a9cfe0581e0ec
bondar5471 <bondar54711745@gmail.com>
2018-12-11 17:17:57 +0200

    Add acceptance tests for create task on day

---
+++ spec/acceptance/create_task_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Create task on day', '
+	click the add task button.
+	open the form enter valid data,
+	click the add task button
+' do
+  given(:day) { create(:day) }
+  scenario 'click on create task on day', js: true do
+    visit day_path(day)
+    click_on 'Add task'
+    fill_in 'task',	with: 'Mytext'
+    fill_in 'date_begin', with: I18n.l(Date.today)
+    fill_in 'date_end', with: I18n.l(Date.today)
+    select 'day', from: 'task_type'
+    sleep 1
+    click_on 'Add'
+    within '#tasklist' do
+      expect(page).to have_content 'Mytext'
+    end
+  end
+
+  scenario 'hide Add task ', js: true do
+    visit day_path(day)
+    click_on 'Add task'
+    sleep 1
+    expect('#taskform').to_not have_content 'Add task'
+  end
+
+  scenario 'visible buton Add task after close form', js: true do
+    visit day_path(day)
+    click_on 'Add task'
+    find('.glyphicon-remove.icoremove').click
+    sleep 1
+    expect(page).to have_content 'Add task'
+  end
+
+  scenario 'not create invalid task', js: true do
+    visit day_path(day)
+    click_on 'Add task'
+    fill_in 'task',	with: ''
+    fill_in 'date_begin', with: I18n.l(Date.today)
+    fill_in 'date_end', with: I18n.l(Date.today)
+    select 'day', from: 'task_type'
+    sleep 1
+    click_on 'Add'
+    within '#tasklist' do
+      expect(page).to have_content ''
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rails'
+require 'selenium/webdriver'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -37,6 +39,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  Capybara.javascript_driver = :selenium
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
