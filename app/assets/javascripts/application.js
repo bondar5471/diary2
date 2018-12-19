0adccd775387486e87fc9d7c89811e431d4aa976
bondar5471 <bondar54711745@gmail.com>
2018-12-19 14:45:20 +0200

    refactoring js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,48 @@
 //= require twitter/bootstrap
 //= require moment
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var success = document.getElementById("success").innerText;
+	var notsuccess = document.getElementById("notsuccess").innerText;
+	var notset = document.getElementById("notset").innerText;
+	var myChart = new Chart(ctx, {
+			type: 'doughnut',
+			data: {
+					labels: ["Not Success", "Success", "Not set"],
+					datasets: [{
+							data: [notsuccess, success, notset],
+							backgroundColor: [
+									'rgb(212, 63, 58)',
+									'rgb(76, 174, 76)',
+									'rgba(135, 204, 250)'
+	
+							],
+							borderColor: [
+									'rgb(150, 57, 57)',
+									'rgb(57, 150, 59)',
+									'rgb(57, 113, 150)'
+							],
+							borderWidth: 2
+					}]
+			},
+			options: {
+				title: {
+					display: true,
+					text: 'Success of the year',
+					fontSize: 20
+				}
+			}
+	})
+});
+$(document).on('turbolinks:load', function() { 
+function update() {
+var clock = document.getElementById('clock');
+clock.innerHTML = moment().format('MMMM D YYYY, HH:mm:ss');
+}
+setInterval(update, 1000);
+});	 
 $(document).on('turbolinks:load', function() {
 var acc = document.getElementsByClassName("accordion");
 var counter;
@@ -34,194 +76,4 @@ for (counter = 0; counter < acc.length; counter++) {
 	} 
 	});
 }
-})
-//selector
-$(document).on('turbolinks:load', function() {
-	$("#task_duration").on('change', function() {
-		var selectorDuration = document.getElementById('task_duration').value
-		if(selectorDuration == "year") {
-				document.getElementById('task_year').style.display = 'inline-block';
-				document.getElementById('date_end').style.display = 'none';
-				document.getElementById('date_field').style.display = 'none';
-		} else {
-				document.getElementById('task_year').style.display = 'none';
-		}
-		if(selectorDuration == "month") {
-				document.getElementById('date_month').style.display = 'inline-block';
-				document.getElementById('task_year').style.display = 'inline-block';
-				document.getElementById('date_end').style.display = 'none';
-				document.getElementById('date_field').style.display = 'none';
-		} else {
-				document.getElementById('date_month').style.display = 'none';
-		}
-		if(selectorDuration == "week") {
-				document.getElementById('task_week').style.display = 'inline-block';
-				document.getElementById('date_end').style.display = 'none';
-				document.getElementById('date_field').style.display = 'none';
-		} else {
-				document.getElementById('task_week').style.display = 'none';
-		}
-		if(selectorDuration == "day") {
-				document.getElementById('date_field').style.display = 'block';
-				document.getElementById('date_end').style.display = 'block';
-				document.getElementById('paragraphDateTask').style.display = 'none';
-		}
-	})
-	$("#sendFormTask").on('click', function() {
-		var selectorDuration = document.getElementById('task_duration').value
-		if(selectorDuration == "year") {
-				debugger;
-				var taskYear = new Date(document.getElementById('task_year').value, 11, 31);
-				document.getElementById('date_end').value =  moment(taskYear).format('YYYY-M-D');
-		}
-		if(selectorDuration == "month") {
-				var taskMonth = new Date(document.getElementById('task_year').value, document.getElementById('date_month').value, 0);
-				document.getElementById('date_end').value = moment(taskMonth).format('YYYY-M-D');
-		}
-		if(selectorDuration == "week") {
-				//calculate last day
-				var taskWeek = new Date("Jan 01, " + document.getElementById('task_year').value + " 01:00:00");
-				var week = taskWeek.getTime() + 604800000 * (document.getElementById('task_week').value - 1);
-				var lastDayWeekTask = new Date(week + 518400000);
-				document.getElementById('date_end').value = moment(lastDayWeekTask).format('YYYY-M-D');
-			}  
-	}),
-	$("#task_week").on('change', function() {
-		var taskWeek = new Date("Jan 01, " + document.getElementById('task_year').value + " 01:00:00");
-		var week = taskWeek.getTime() + 604800000 * (document.getElementById('task_week').value - 1);
-		var lastDayWeekTask = new Date(week + 518400000);
-		var dateDayWeekTask = document.getElementById('paragraphDateTask');
-		dateDayWeekTask.style.display = "block";
-		dateDayWeekTask.innerText = moment(lastDayWeekTask).format('MMMM D YYYY');
-	})	
- });
- //create task
- $(document).on('turbolinks:load', function() {
- 
-   $(".sendtask").click(function(){  
-     debugger;
-     var task = document.getElementById("task").value; 
-     var current_day = $(this).parents('.task-container');
-     var container = document.getElementById('tasklist');
-     var idDay = $(current_day).attr('data-day_id');
-     var date_end = document.getElementById("date_end").value;
-     var type_task = document.getElementById("task_duration").value
-     var taskYear = document.getElementById('task_year').value
-     var taskMoth = document.getElementById('date_month').value
-     var taskWeek = document.getElementById('task_week').value
-     $.ajax({
-         url: "/days/:day_id/tasks".replace(":day_id", idDay),
-         type: "POST",
-         beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-         dataType: "json",
-         data: { task: { list: task, date_end: date_end, duration: type_task},
-         dataTask:{task_year: taskYear, date_month: taskMoth, task_week: taskWeek} },
-         success: function(data) {
-           addNewTask(data, container);           
-         },
-         error:  
-           notValidTask()
-     });
-   });
- });
- 
- function addNewTask(task, tasksListDiv) {
-   $('#task').css('border-color','seagreen');
-   $('#date_end').css('border-color','seagreen');
-   var link = document.createElement('a');
-   link.className = "glyphicon glyphicon-trash"
-   link.setAttribute ("data-method", "delete");
-   link.href = ("/days/:day_id/tasks/".replace(":day_id", task.day_id) + task.id);
-   link.setAttribute("data-remote", "true");
-   
-   var paragraph = document.createElement('p')
-   paragraph.innerText = task.list;
-   paragraph.style.fontWeight = "bold";
-   paragraph.style.display = "inline-block";
-   
-   var onetask = document.createElement('div')
-   onetask.className = "onetask";
-   onetask.className = "bluetask";
-   onetask.style.borderRadius = "5px";
-   onetask.style.marginTop = "2px";
-   onetask.style.paddingLeft = "6px";
-  
-   onetask.appendChild(paragraph);
-   onetask.appendChild(link);
- 
-   tasksListDiv.appendChild(onetask);
- 
-   $('#task').val('');
- 
- }
- function notValidTask() {
-   $('#task').each(function(){
-     if(!$(this).val() || $(this).val() === ""){
-       $(this).css('border-color','red');
-       send = false;
-       }
-     })
-     $('#date_end').each(function(){
-       if(!$(this).val() || $(this).val() === ""){
-         $(this).css('border-color','red');
-         send = false;
-         }
-       }) 
-   }
- 
- //vicible form task
- $(document).on('turbolinks:load', function() {
- $("#taskButton").click(function(e){
-   e.preventDefault();
-   $("#taskform").show();
-   $("#taskButton").toggle();
- });
- });
- $(document).on('turbolinks:load', function() {
-   $("#hideform").click(function(){
-     $("#taskform").hide();
-     $("#taskButton").show();
-     
-   });
-   });
-   $(document).on('turbolinks:load', function() {
-     var ctx = document.getElementById("myChart").getContext('2d');
-     var success = document.getElementById("success").innerText;
-     var notsuccess = document.getElementById("notsuccess").innerText;
-     var notset = document.getElementById("notset").innerText;
-     var myChart = new Chart(ctx, {
-         type: 'doughnut',
-         data: {
-             labels: ["Not Success", "Success", "Not set"],
-             datasets: [{
-                 data: [notsuccess, success, notset],
-                 backgroundColor: [
-                     'rgb(212, 63, 58)',
-                     'rgb(76, 174, 76)',
-                     'rgba(135, 204, 250)'
-     
-                 ],
-                 borderColor: [
-                     'rgb(150, 57, 57)',
-                     'rgb(57, 150, 59)',
-                     'rgb(57, 113, 150)'
-                 ],
-                 borderWidth: 2
-             }]
-         },
-         options: {
-           title: {
-             display: true,
-             text: 'Success of the year',
-             fontSize: 20
-           }
-         }
-     })
-	 });
-	$(document).on('turbolinks:load', function() { 
-	 function update() {
-		var clock = document.getElementById('clock');
-		clock.innerHTML = moment().format('MMMM D YYYY, HH:mm:ss');
-		}
-		setInterval(update, 1000);
-	});	 
+})	
--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,165 +1,149 @@
-
-// $(document).on('turbolinks:load', function() {
-// //accordion
-// 	var acc = document.getElementsByClassName("accordion");
-// var counter;
-
-// for (counter = 0; counter < acc.length; counter++) {
-// 	acc[counter].addEventListener("click", function() {
-// 	this.classList.toggle("active");
-// 	var panel = this.nextElementSibling;
-// 	if (panel.style.maxHeight){
-// 		panel.style.maxHeight = null;
-// 	} else {
-// 		panel.style.maxHeight = panel.scrollHeight + "px";
-// 	} 
-// 	});
-// }
-//   $("#taskButton").click(function(e){
-// 		e.preventDefault();
-// 		$("#taskform").show();
-// 		$("#taskButton").toggle();
-//   })
-//   $("#hideform").click(function(){
-//       $("#taskform").hide();
-//       $("#taskButton").show();	
-//   })
-//   //create task
-//   $(".sendtask").click(function(){  
-//       var task = document.getElementById("task").value; 
-//       var current_day = $(this).parents('.task-container');
-//       var container = document.getElementById('tasklist');
-//       var idDay = $(current_day).attr('data-day_id');
-//       var date_end = document.getElementById("date_end").value;
-//       var type_task = document.getElementById("task_duration").value
-//       $.ajax({
-//               url: "/days/:day_id/tasks".replace(":day_id", idDay),
-//               type: "POST",
-//               beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-//               dataType: "json",
-//               data: { task: { list: task, date_end: date_end, duration: type_task} },
-//               success: function(data) {
-//                   addNewTask(data, container);           
-//               },
-//               error:  
-//                   notValidTask()
-//   })
-//   })
-//   function addNewTask(task, tasksListDiv) {
-//       $('#task').css('border-color','seagreen');
-//       $('#date_end').css('border-color','seagreen');
-//       var link = document.createElement('a');
-//       link.className = "glyphicon glyphicon-trash"
-//       link.setAttribute ("data-method", "delete");
-//       link.href = ("/days/:day_id/tasks/".replace(":day_id", task.day_id) + task.id);
-//       link.setAttribute("data-remote", "true");
+$(document).on('turbolinks:load', function() {
+  $("#taskButton").click(function(e){
+		e.preventDefault();
+		$("#taskform").show();
+		$("#taskButton").toggle();
+  })
+  $("#hideform").click(function(){
+      $("#taskform").hide();
+      $("#taskButton").show();	
+  })
+  //create task
+  $(".sendtask").click(function(){  
+      var task = document.getElementById("task").value; 
+      var current_day = $(this).parents('.task-container');
+      var container = document.getElementById('tasklist');
+      var idDay = $(current_day).attr('data-day_id');
+      var date_end = document.getElementById("date_end").value;
+      var type_task = document.getElementById("task_duration").value
+      $.ajax({
+              url: "/days/:day_id/tasks".replace(":day_id", idDay),
+              type: "POST",
+              beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+              dataType: "json",
+              data: { task: { list: task, date_end: date_end, duration: type_task} },
+              success: function(data) {
+                  addNewTask(data, container);           
+              },
+              error:  
+                  notValidTask()
+  })
+  })
+  function addNewTask(task, tasksListDiv) {
+      $('#task').css('border-color','seagreen');
+      $('#date_end').css('border-color','seagreen');
+      var link = document.createElement('a');
+      link.className = "glyphicon glyphicon-trash"
+      link.setAttribute ("data-method", "delete");
+      link.href = ("/days/:day_id/tasks/".replace(":day_id", task.day_id) + task.id);
+      link.setAttribute("data-remote", "true");
       
-//       var paragraph = document.createElement('p')
-//       paragraph.innerText = task.list;
-//       paragraph.style.fontWeight = "bold";
-//       paragraph.style.display = "inline-block";
+      var paragraph = document.createElement('p')
+      paragraph.innerText = task.list;
+      paragraph.style.fontWeight = "bold";
+      paragraph.style.display = "inline-block";
       
-//       var onetask = document.createElement('div')
-//       onetask.className = "onetask";
-//       onetask.className = "bluetask";
-//       onetask.style.borderRadius = "5px";
-//       onetask.style.marginTop = "2px";
-//       onetask.style.paddingLeft = "6px";
+      var onetask = document.createElement('div')
+      onetask.className = "onetask";
+      onetask.className = "bluetask";
+      onetask.style.borderRadius = "5px";
+      onetask.style.marginTop = "2px";
+      onetask.style.paddingLeft = "6px";
       
-//       onetask.appendChild(paragraph);
-//       onetask.appendChild(link);
+      onetask.appendChild(paragraph);
+      onetask.appendChild(link);
   
-//       tasksListDiv.appendChild(onetask);
+      tasksListDiv.appendChild(onetask);
   
-//       $('#task').val('');
+      $('#task').val('');
   
-//   }
-//   function notValidTask() {
-//       $('#task').each(function(){
-//           if(!$(this).val() || $(this).val() === ""){
-//               $(this).css('border-color','red');
-//               send = false;
-//               }
-//           })
-//           $('#date_end').each(function(){
-//               if(!$(this).val() || $(this).val() === ""){
-//                   $(this).css('border-color','red');
-//                   send = false;
-//                   }
-//               }) 
-// 			}
-// 	var accordion = document.getElementsByClassName("accordion");
-// 	var counter;
+  }
+  function notValidTask() {
+      $('#task').each(function(){
+          if(!$(this).val() || $(this).val() === ""){
+              $(this).css('border-color','red');
+              send = false;
+              }
+          })
+          $('#date_end').each(function(){
+              if(!$(this).val() || $(this).val() === ""){
+                  $(this).css('border-color','red');
+                  send = false;
+                  }
+              }) 
+			}
+	var accordion = document.getElementsByClassName("accordion");
+	var counter;
 	
-// 	for (counter = 0; counter < accordion.length; counter++) {
-// 		accordion[counter].addEventListener("click", function() {
-// 			this.classList.toggle("active");
-// 			var panel = this.nextElementSibling;
-// 			if (panel.style.maxHeight){
-// 				panel.style.maxHeight = null;
-// 			} else {
-// 				panel.style.maxHeight = panel.scrollHeight + "px";
-// 			} 
-// 		});
-// 	}
+	for (counter = 0; counter < accordion.length; counter++) {
+		accordion[counter].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight){
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			} 
+		});
+	}
 
-// 	//selectors task
+	//selectors task
 	
-// 	$("#task_duration").on('change', function() {
-// 		var selectorDuration = document.getElementById('task_duration').value
-// 		if(selectorDuration == "year") {
-// 				document.getElementById('task_year').style.display = 'inline-block';
-// 				document.getElementById('date_end').style.display = 'none';
-// 				document.getElementById('date_field').style.display = 'none';
-// 		} else {
-// 				document.getElementById('task_year').style.display = 'none';
-// 		}
-// 		if(selectorDuration == "month") {
-// 				document.getElementById('date_month').style.display = 'inline-block';
-// 				document.getElementById('task_year').style.display = 'inline-block';
-// 				document.getElementById('date_end').style.display = 'none';
-// 				document.getElementById('date_field').style.display = 'none';
-// 		} else {
-// 				document.getElementById('date_month').style.display = 'none';
-// 		}
-// 		if(selectorDuration == "week") {
-// 				document.getElementById('task_week').style.display = 'inline-block';
-// 				document.getElementById('date_end').style.display = 'none';
-// 				document.getElementById('date_field').style.display = 'none';
-// 		} else {
-// 				document.getElementById('task_week').style.display = 'none';
-// 		}
-// 		if(selectorDuration == "day") {
-// 				document.getElementById('date_field').style.display = 'block';
-// 				document.getElementById('date_end').style.display = 'block';
-// 				document.getElementById('paragraphDateTask').style.display = 'none';
-// 		}
-// 	})
-// 	$("#sendFormTask").on('click', function() {
-// 		var selectorDuration = document.getElementById('task_duration').value
-// 		if(selectorDuration == "year") {
-// 				debugger;
-// 				var taskYear = new Date(document.getElementById('task_year').value, 11, 31);
-// 				document.getElementById('date_end').value =  moment(taskYear).format('YYYY-M-D');
-// 		}
-// 		if(selectorDuration == "month") {
-// 				var taskMonth = new Date(document.getElementById('task_year').value, document.getElementById('date_month').value, 0);
-// 				document.getElementById('date_end').value = moment(taskMonth).format('YYYY-M-D');
-// 		}
-// 		if(selectorDuration == "week") {
-// 				//calculate last day
-// 				var taskWeek = new Date("Jan 01, " + document.getElementById('task_year').value + " 01:00:00");
-// 				var week = taskWeek.getTime() + 604800000 * (document.getElementById('task_week').value - 1);
-// 				var lastDayWeekTask = new Date(week + 518400000);
-// 				document.getElementById('date_end').value = moment(lastDayWeekTask).format('YYYY-M-D');
-// 			}  
-// 	}),
-// 	$("#task_week").on('change', function() {
-// 		var taskWeek = new Date("Jan 01, " + document.getElementById('task_year').value + " 01:00:00");
-// 		var week = taskWeek.getTime() + 604800000 * (document.getElementById('task_week').value - 1);
-// 		var lastDayWeekTask = new Date(week + 518400000);
-// 		var dateDayWeekTask = document.getElementById('paragraphDateTask');
-// 		dateDayWeekTask.style.display = "block";
-// 		dateDayWeekTask.innerText = moment(lastDayWeekTask).format('MMMM D YYYY');
-// 	})	
-// });
+	$("#task_duration").on('change', function() {
+		var selectorDuration = document.getElementById('task_duration').value
+		if(selectorDuration == "year") {
+				document.getElementById('task_year').style.display = 'inline-block';
+				document.getElementById('date_end').style.display = 'none';
+				document.getElementById('date_field').style.display = 'none';
+		} else {
+				document.getElementById('task_year').style.display = 'none';
+		}
+		if(selectorDuration == "month") {
+				document.getElementById('date_month').style.display = 'inline-block';
+				document.getElementById('task_year').style.display = 'inline-block';
+				document.getElementById('date_end').style.display = 'none';
+				document.getElementById('date_field').style.display = 'none';
+		} else {
+				document.getElementById('date_month').style.display = 'none';
+		}
+		if(selectorDuration == "week") {
+				document.getElementById('task_week').style.display = 'inline-block';
+				document.getElementById('date_end').style.display = 'none';
+				document.getElementById('date_field').style.display = 'none';
+		} else {
+				document.getElementById('task_week').style.display = 'none';
+		}
+		if(selectorDuration == "day") {
+				document.getElementById('date_field').style.display = 'block';
+				document.getElementById('date_end').style.display = 'block';
+				document.getElementById('paragraphDateTask').style.display = 'none';
+		}
+	})
+	$("#sendFormTask").on('click', function() {
+		var selectorDuration = document.getElementById('task_duration').value
+		if(selectorDuration == "year") {
+				debugger;
+				var taskYear = new Date(document.getElementById('task_year').value, 11, 31);
+				document.getElementById('date_end').value =  moment(taskYear).format('YYYY-M-D');
+		}
+		if(selectorDuration == "month") {
+				var taskMonth = new Date(document.getElementById('task_year').value, document.getElementById('date_month').value, 0);
+				document.getElementById('date_end').value = moment(taskMonth).format('YYYY-M-D');
+		}
+		if(selectorDuration == "week") {
+				//calculate last day
+				var taskWeek = new Date("Jan 01, " + document.getElementById('task_year').value + " 01:00:00");
+				var week = taskWeek.getTime() + 604800000 * (document.getElementById('task_week').value - 1);
+				var lastDayWeekTask = new Date(week + 518400000);
+				document.getElementById('date_end').value = moment(lastDayWeekTask).format('YYYY-M-D');
+			}  
+	}),
+	$("#task_week").on('change', function() {
+		var taskWeek = new Date("Jan 01, " + document.getElementById('task_year').value + " 01:00:00");
+		var week = taskWeek.getTime() + 604800000 * (document.getElementById('task_week').value - 1);
+		var lastDayWeekTask = new Date(week + 518400000);
+		var dateDayWeekTask = document.getElementById('paragraphDateTask');
+		dateDayWeekTask.style.display = "block";
+		dateDayWeekTask.innerText = moment(lastDayWeekTask).format('MMMM D YYYY');
+	})	
+});
