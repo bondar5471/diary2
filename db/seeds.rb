a8eeeed1a6d9cb54a4dba86cad48bc86734c84cf
Sergey Bondarenko <sbondarenko@sbondarenko-lx.nixsolutions.com>
2018-12-28 12:45:37 +0200

    fix style for display

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 dates = Time.zone.today.beginning_of_year - 1.day
 Day.destroy_all
 Time.zone.today.end_of_year.yday.times do
@@ -15,7 +5,7 @@ Time.zone.today.end_of_year.yday.times do
     date: dates += 1.day,
     successful: nil,
     report: nil,
-    user_id: 1
+    user_id: nil
   )
 end
 p "Created #{Day.count} days"
