c6124727e10b58cee700317a763af33bb765a9ad
bondar5471 <bondar54711745@gmail.com>
2019-01-08 23:07:46 +0200

    fix js

--- app/assets/javascripts/days.js
+++ app/assets/javascripts/days.js
@@ -1,41 +1,19 @@
-// $(document).on('turbolinks:load', function() {
-//     var ctx = document.getElementById("myChart").getContext('2d');
-//     var success = document.getElementById("success").innerText;
-//     var notsuccess = document.getElementById("notsuccess").innerText;
-//     var notset = document.getElementById("notset").innerText;
-//     var myChart = new Chart(ctx, {
-//         type: 'doughnut',
-//         data: {
-//             labels: ["Not Success", "Success", "Not set"],
-//             datasets: [{
-//                 data: [notsuccess, success, notset],
-//                 backgroundColor: [
-//                     'rgb(212, 63, 58)',
-//                     'rgb(76, 174, 76)',
-//                     'rgba(135, 204, 250)'
-    
-//                 ],
-//                 borderColor: [
-//                     'rgb(150, 57, 57)',
-//                     'rgb(57, 150, 59)',
-//                     'rgb(57, 113, 150)'
-//                 ],
-//                 borderWidth: 2
-//             }]
-//         },
-//         options: {
-//           title: {
-//             display: true,
-//             text: 'Success of the year',
-//             fontSize: 20
-//           }
-//         }
-//     })
+$(document).on('turbolinks:load', function() {
+//open foto
+var modal = document.getElementById('myModal');
+	
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 
-// //clock
-//   function update() {
-//   var clock = document.getElementById('clock');
-//   clock.innerHTML = moment().format('MMMM D YYYY, HH:mm:ss');
-//   }
-//   setInterval(update, 1000);
-// });
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+})
