da90e96fab297a307f82cc5fb04425548e915b11
bondar5471 <bondar54711745@gmail.com>
2019-01-14 12:45:27 +0200

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,8 @@
 time_create_days = Time.now
 Day.delete_all
 today = Time.zone.today
-records = (today.beginning_of_year..today.end_of_year).to_a.map { |date| Day.new(date: date, user_id: 1) }
+dates = (today.beginning_of_year..today.end_of_year).to_a
+records = dates.map { |date| Day.new(date: date, user_id: 1) }
 ActiveRecord::Base.transaction { records.each(&:save) }
 endtime = Time.now - time_create_days
 p "Time creating#{endtime}"
