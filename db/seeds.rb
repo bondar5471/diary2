89ce5625b8c08ee478177afda3128a8568496d65
bondar5471 <bondar54711745@gmail.com>
2019-01-14 13:13:49 +0200

    Edit sql raquest create day seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
-
-# dates = Time.zone.today.beginning_of_year - 1.day
-time_create_days = Time.now
 Day.delete_all
 today = Time.zone.today
 dates = (today.beginning_of_year..today.end_of_year).to_a
-records = dates.map { |date| Day.new(date: date, user_id: 1) }
-ActiveRecord::Base.transaction { records.each(&:save) }
+time_create_days = Time.now
+sql_dates = dates.map { |d| "('#{d}', '#{1}', '#{Time.now.to_s}', '#{Time.now.to_s}')" }.join(", ")
+ActiveRecord::Base.connection.execute("INSERT INTO days (date, user_id, created_at, updated_at) VALUES #{sql_dates}")
 endtime = Time.now - time_create_days
-p "Time creating#{endtime}"
-p "Created #{Day.count} days"
+p "Time creating #{endtime}"
+p "Days count #{Day.count}"
