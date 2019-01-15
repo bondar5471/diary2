2eea31c72b85f09d72e8a16aa3942989d2ddad0e
bondar5471 <bondar54711745@gmail.com>
2019-01-15 11:29:35 +0200

    fix feature test

--- spec/feature/tasks/accordion_spec.rb
+++ spec/feature/tasks/accordion_spec.rb
@@ -4,14 +4,17 @@ require 'rails_helper'
 feature 'accordion task panel' do
   given(:day) { create(:day) }
   given!(:user) { create(:user) }
-  given!(:task) { create(:task) }
   before do
     visit new_user_session_path
     fill_in 'user_email',	with: user.email
     fill_in 'user_password', with: user.password
     click_on 'Log in'
     visit day_path(day)
-    click_on 'Task on day'
+    click_on 'Add task'
+    fill_in 'task',	with: 'MyTask'
+    fill_in 'date_end', with: Time.zone.today
+    select 'day', from: 'task_duration'
+    click_on 'Add'
   end
   scenario 'Accordion Panel Display', js: true do
     expect(page).to have_css '.panel'
